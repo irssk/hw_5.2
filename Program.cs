3ad2cbb178980583d01a62ade49a7666389075fe
Ira <ira350520@gmail.com>
2025-03-20 18:49:38 +0200

    completed homework

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out double number))
+        {
+            Console.WriteLine($"You entered a number: {number}");
+        }
+        else
+        {
+            Console.WriteLine("Error: The value entered is not a number.");
+        }
+    }
+}
